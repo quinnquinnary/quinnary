0767f2c6a9020bc5db3e104e3ba64e90d3df6a5b
quinnquinnary <quinnquinnary@gmail.com>
2025-10-18 18:11:05 -0700

    Update sketch.js

--- portfolio/sketch.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector(".cursor")
-
-document.addEventListener("mousemove", (event) => {
-    const {width, height} = cursor.getBoundingClientRect()
-    cursor.style.left = '${event.clientX - width / 2}px'
-    cursor.style.left = '${event.clientY - height / 2}px'
-})
